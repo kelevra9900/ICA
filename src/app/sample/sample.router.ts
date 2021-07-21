2f49056f69025da58eeb56ad63aa112b4f9a3ddb
Rogelio Torres <torresroger445@gmail.com>
2021-07-21 07:52:52 -0500

    sample routes to get Agua santa station

--- src/app/sample/sample.router.ts
+++ src/app/sample/sample.router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import axios from 'axios';
 
 // Export module for registering router in express app
 export const router: Router = Router();
@@ -10,22 +11,30 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
-  res.status(200).send({
-    message: "POST request from sample router"
-  });
+//Solo se obtiene la información del verificentro se Agua Santa 
+router.get("/recomendaciones", async (req, res, next) => {
+
+  var found:any = [],
+  rxp = /(?<=series:\s).*?(?=\s*}])/gs,
+  curMatch;
+
+  try{
+    const response = await axios.get("http://calidaddelaire.puebla.gob.mx/models/grafico_ICA_SANTA.php");
+    while(curMatch = rxp.exec(response.data)) {
+      found.push(curMatch[0]);
+    }
+
+    //reemplaza las comillas simples con comillas dobles
+    var doubleQuotes = found.toString().replace(/'/g, '"')
+
+    //extrae los espacios /t /r  /n etc
+    let data = doubleQuotes.replace(/(\r\n|\n|\r|\t)/gm, "");
+    res.status(200).json({
+      success: true,
+      data
+    });
+  }catch(e){
+    console.log('error --->', e);
+    next(e);
+  }
 });
-
-router.put("/", (req, res) => {
-  res.status(200).send({
-    message: "PUT request from sample router"
-  });
-});
-
-router.delete("/", (req, res) => {
-  res.status(200).send({
-    message: "DELETE request from sample router"
-  });
-});
-
-
